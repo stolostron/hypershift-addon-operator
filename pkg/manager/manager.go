659fe37fea0e74156c5e4e9dc69a8e247c28ca1e
Jian Zhu <36154065+zhujian7@users.noreply.github.com>
2022-03-01 23:33:10 +0800

    Disable install strategy all (#11)
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -67,7 +67,6 @@ func NewManagerCommand(componentName string, log logr.Logger) *cobra.Command {
 		agentAddon, err := addonfactory.NewAgentAddonFactory(componentName, fs, templatePath).
 			WithGetValuesFuncs(getValueForAgentTemplate, addonfactory.GetValuesFromAddonAnnotation).
 			WithAgentRegistrationOption(registrationOption).
-			WithInstallStrategy(frameworkagent.InstallAllStrategy(util.AgentInstallationNamespace)).
 			BuildTemplateAgentAddon()
 		if err != nil {
 			log.Error(err, "failed to build agent")
