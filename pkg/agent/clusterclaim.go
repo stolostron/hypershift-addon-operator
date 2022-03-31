bac78c6aabb3550423956a1b89c2310cb9bd618f
Jian Zhu <36154065+zhujian7@users.noreply.github.com>
2022-03-31 09:27:25 +0800

    Add cluster claim for hypershift clusters (#27)
    * Add cluster claim for hypershift clusters
    
    Signed-off-by: zhujian <jiazhu@redhat.com>
    
    * Rename claim to hosting cluster
    
    Signed-off-by: zhujian <jiazhu@redhat.com>

---
+++ pkg/agent/clusterclaim.go
@@ -0,0 +1,97 @@
+package agent
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	"github.com/stolostron/hypershift-addon-operator/pkg/util"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	clusterclientset "open-cluster-management.io/api/client/cluster/clientset/versioned"
+
+	clusterv1alpha1 "open-cluster-management.io/api/cluster/v1alpha1"
+)
+
+const (
+	// labelExcludeBackup is true for the local-cluster will not be backed up into velero
+	labelExcludeBackup = "velero.io/exclude-from-backup"
+
+	hypershiftManagementClusterClaimKey = "hostingcluster.hypershift.openshift.io"
+	hypershiftHostedClusterClaimKey     = "hostedcluster.hypershift.openshift.io"
+)
+
+func newClusterClaim(name, value string) *clusterv1alpha1.ClusterClaim {
+	return &clusterv1alpha1.ClusterClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   name,
+			Labels: map[string]string{labelExcludeBackup: "true"},
+		},
+		Spec: clusterv1alpha1.ClusterClaimSpec{
+			Value: value,
+		},
+	}
+}
+
+func createOrUpdate(ctx context.Context, client clusterclientset.Interface, newClaim *clusterv1alpha1.ClusterClaim) error {
+	oldClaim, err := client.ClusterV1alpha1().ClusterClaims().Get(ctx, newClaim.Name, metav1.GetOptions{})
+	switch {
+	case errors.IsNotFound(err):
+		_, err := client.ClusterV1alpha1().ClusterClaims().Create(ctx, newClaim, metav1.CreateOptions{})
+		if err != nil {
+			return fmt.Errorf("unable to create ClusterClaim: %v, %w", newClaim, err)
+		}
+	case err != nil:
+		return fmt.Errorf("unable to get ClusterClaim %q: %w", newClaim.Name, err)
+	case !reflect.DeepEqual(oldClaim.Spec, newClaim.Spec):
+		oldClaim.Spec = newClaim.Spec
+		_, err := client.ClusterV1alpha1().ClusterClaims().Update(ctx, oldClaim, metav1.UpdateOptions{})
+		if err != nil {
+			return fmt.Errorf("unable to update ClusterClaim %q: %w", oldClaim.Name, err)
+		}
+	}
+	return nil
+}
+
+func (c *agentController) createManagementClusterClaim(ctx context.Context) error {
+	managementClaim := newClusterClaim(hypershiftManagementClusterClaimKey, "true")
+	return createOrUpdate(ctx, c.spokeClustersClient, managementClaim)
+}
+
+func (c *agentController) createHostedClusterClaim(ctx context.Context, secretKey types.NamespacedName,
+	generateClusterClientFromSecret func(secret *corev1.Secret) (clusterclientset.Interface, error)) error {
+	secret := &corev1.Secret{}
+	err := c.spokeClient.Get(ctx, secretKey, secret)
+	if err != nil {
+		return fmt.Errorf("unable to get hosted secret %s/%s, err: %w", secretKey.Namespace, secretKey.Name, err)
+	}
+
+	clusterClient, err := generateClusterClientFromSecret(secret)
+	if err != nil {
+		return fmt.Errorf("failed to create spoke clusters client, err: %w", err)
+	}
+
+	hostedClaim := newClusterClaim(hypershiftHostedClusterClaimKey, "true")
+	err = createOrUpdate(ctx, clusterClient, hostedClaim)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func generateClusterClientFromSecret(secret *corev1.Secret) (clusterclientset.Interface, error) {
+	config, err := util.GenerateClientConfigFromSecret(secret)
+	if err != nil {
+		return nil, fmt.Errorf("unable to generate client config from secret: %w", err)
+	}
+
+	clusterClient, err := clusterclientset.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create spoke clusters client, err: %w", err)
+	}
+
+	return clusterClient, nil
+}
---
+++ pkg/agent/clusterclaim_test.go
@@ -0,0 +1,133 @@
+package agent
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/openshift/library-go/pkg/operator/events/eventstesting"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	hyperv1alpha1 "github.com/openshift/hypershift/api/v1alpha1"
+	clusterclientset "open-cluster-management.io/api/client/cluster/clientset/versioned"
+	clustercsfake "open-cluster-management.io/api/client/cluster/clientset/versioned/fake"
+)
+
+var (
+	testscheme = clientgoscheme.Scheme
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(testscheme))
+	utilruntime.Must(hyperv1alpha1.AddToScheme(testscheme))
+}
+
+func TestCreateManagementClusterClaim(t *testing.T) {
+	cases := []struct {
+		name         string
+		validateFunc func(t *testing.T, clusterClient clusterclientset.Interface)
+	}{
+		{
+			name: "create management cluster claim successfully",
+			validateFunc: func(t *testing.T, clusterClient clusterclientset.Interface) {
+				cc, err := clusterClient.ClusterV1alpha1().ClusterClaims().Get(context.TODO(), hypershiftManagementClusterClaimKey, metav1.GetOptions{})
+				assert.Nil(t, err)
+				assert.Equal(t, "true", cc.Spec.Value)
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			fakeClusterCS := clustercsfake.NewSimpleClientset()
+			ctrl := &agentController{
+				spokeClustersClient: fakeClusterCS,
+				recorder:            eventstesting.NewTestingEventRecorder(t),
+			}
+
+			err := ctrl.createManagementClusterClaim(context.TODO())
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			c.validateFunc(t, fakeClusterCS)
+		})
+	}
+}
+
+func TestCreateHostedClusterClaim(t *testing.T) {
+	cases := []struct {
+		name                   string
+		startObjs              []ctrlclient.Object
+		hostedclusterName      string
+		hostedclusterNamespace string
+		expectErr              string
+		validateFunc           func(t *testing.T, runtimeClient ctrlclient.Client, clusterClient clusterclientset.Interface)
+	}{
+		{
+			name:                   "create hosted cluster claim successfully",
+			hostedclusterName:      "hc1",
+			hostedclusterNamespace: "clusters",
+			startObjs: []ctrlclient.Object{
+				&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "hc1-admin-kubeconfig",
+						Namespace: "clusters",
+					},
+					Data: map[string][]byte{
+						"kubeconfig": []byte("test"),
+					},
+				},
+			},
+			validateFunc: func(t *testing.T, runtimeClient ctrlclient.Client, clusterClient clusterclientset.Interface) {
+				cc, err := clusterClient.ClusterV1alpha1().ClusterClaims().Get(context.TODO(), hypershiftHostedClusterClaimKey, metav1.GetOptions{})
+				assert.Nil(t, err)
+				assert.Equal(t, "true", cc.Spec.Value)
+			},
+		},
+		{
+			name:                   "create hosted cluster claim failed, secret not found",
+			hostedclusterName:      "hc1",
+			hostedclusterNamespace: "clusters",
+			expectErr:              "not found",
+			validateFunc: func(t *testing.T, runtimeClient ctrlclient.Client, clusterClient clusterclientset.Interface) {
+				_, err := clusterClient.ClusterV1alpha1().ClusterClaims().Get(context.TODO(), hypershiftHostedClusterClaimKey, metav1.GetOptions{})
+				assert.True(t, errors.IsNotFound(err))
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			client := fake.NewClientBuilder().WithScheme(testscheme).WithObjects(c.startObjs...).Build()
+			ctrl := &agentController{
+				spokeClient: client,
+				recorder:    eventstesting.NewTestingEventRecorder(t),
+			}
+
+			fakeClusterCS := clustercsfake.NewSimpleClientset()
+			err := ctrl.createHostedClusterClaim(context.TODO(), types.NamespacedName{Namespace: c.hostedclusterNamespace, Name: fmt.Sprintf("%s-admin-kubeconfig", c.hostedclusterName)},
+				func(secret *corev1.Secret) (clusterclientset.Interface, error) {
+					return fakeClusterCS, nil
+				})
+			if len(c.expectErr) == 0 && err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			if len(c.expectErr) != 0 && (err == nil || !strings.Contains(err.Error(), c.expectErr)) {
+				t.Errorf("expected error: %v, but got: %v", c.expectErr, err)
+			}
+
+			c.validateFunc(t, ctrl.spokeClient, fakeClusterCS)
+		})
+	}
+}
