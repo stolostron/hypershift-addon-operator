1f745fbf00262e371d8298a745eb4a8e56496704
zhujian <jiazhu@redhat.com>
2022-04-21 02:58:06 +0000

    Fix e2e issue
    Signed-off-by: zhujian <jiazhu@redhat.com>

---
+++ hack/install_ocm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xv
+set -o nounset
+set -o pipefail
+
+CLUSTERADM=${CLUSTERADM:-clusteradm}
+KUBECTL=${KUBECTL:-kubectl}
+_managed_cluster_name="local-cluster"
+
+$CLUSTERADM init --output-join-command-file join.sh --wait
+sh -c "$(cat join.sh) $_managed_cluster_name"
+$CLUSTERADM accept --clusters $_managed_cluster_name --wait 30
+$KUBECTL wait --for=condition=ManagedClusterConditionAvailable managedcluster/$_managed_cluster_name --timeout=60s
