3088d2c29e5d8d5e52308b35657c38ee816167f2
Jian Zhu <36154065+zhujian7@users.noreply.github.com>
2022-05-07 16:10:18 +0800

    Add image pull secret for addon agent (#40)
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- pkg/util/constant.go
+++ pkg/util/constant.go
@@ -17,7 +17,7 @@ const (
 	// AgentInstallationNamespace is the namespace on the managed cluster to install the addon agent.
 	AgentInstallationNamespace = "open-cluster-management-agent-addon"
 
-	MulticlusterHubPullSecret = "open-cluster-management-image-pull-credentials"
+	MulticlusterEnginePullSecret = "open-cluster-management-image-pull-credentials"
 
 	HypershiftDownstreamOverride = "hypershift-operator-imagestream"
 	HypershiftOverrideKey        = "imagestream"
