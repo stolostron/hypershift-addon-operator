3ea1b7d5ac948e5d3358c8fdaf53e131259cdb47
Roke Jung <roke@redhat.com>
2022-12-15 16:49:35 -0500

    more test coverage
    Signed-off-by: Roke Jung <roke@redhat.com>

--- pkg/manager/manager_test.go
+++ pkg/manager/manager_test.go
@@ -1,6 +1,7 @@
 package manager
 
 import (
+	"context"
 	"testing"
 
 	"github.com/go-logr/zapr"
@@ -8,14 +9,18 @@ import (
 	routev1 "github.com/openshift/api/route/v1"
 	"github.com/openshift/library-go/pkg/controller/controllercmd"
 	operatorsv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
+	"github.com/stolostron/hypershift-addon-operator/pkg/util"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	addonv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
 	fakeaddon "open-cluster-management.io/api/client/addon/clientset/versioned/fake"
+	clusterv1 "open-cluster-management.io/api/cluster/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
@@ -60,6 +65,50 @@ func Test_getAgentAddon(t *testing.T) {
 			assert.NotNil(t, got, "agent addon is not nil")
 		})
 	}
+
+	cluster := &clusterv1.ManagedCluster{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ManagedCluster",
+			APIVersion: "cluster.open-cluster-management.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cluster1",
+		},
+	}
+
+	addon := &addonv1alpha1.ManagedClusterAddOn{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ManagedClusterAddOn",
+			APIVersion: "addon.open-cluster-management.io/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "cluster1addon",
+			Namespace: "cluster1",
+		},
+	}
+
+	o.withOverride = true
+
+	_, err := o.getValueForAgentTemplate(cluster, addon)
+	assert.NotNil(t, err, "err not nil because the override configmap does not exist")
+
+	overrideCM := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      util.HypershiftDownstreamOverride,
+			Namespace: o.operatorNamespace,
+		},
+		Data: map[string]string{"test": "test"},
+	}
+	err = o.Client.Create(context.TODO(), overrideCM)
+	assert.Nil(t, err, "err nil when override configmap is created successfull")
+
+	_, err = o.getValueForAgentTemplate(cluster, addon)
+	assert.Nil(t, err, "err nil because the override configmap exists now and can generate the addon chart values")
+
 }
 
 func initClient() client.Client {
@@ -70,6 +119,8 @@ func initClient() client.Client {
 	consolev1.AddToScheme(scheme)
 	appsv1.AddToScheme(scheme)
 	rbacv1.AddToScheme(scheme)
+	clusterv1.AddToScheme(scheme)
+	addonv1alpha1.AddToScheme(scheme)
 
 	ncb := fake.NewClientBuilder()
 	ncb.WithScheme(scheme)
