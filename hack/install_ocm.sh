726f2af395ff4cd5c52227bb6c1aa2f708e5d068
Mike Ng <ming@redhat.com>
2023-04-27 10:52:40 -0400

    Fix e2e by downloading and running the clusteradm binary instead of building it from source
    Signed-off-by: Mike Ng <ming@redhat.com>

--- hack/install_ocm.sh
+++ hack/install_ocm.sh
@@ -8,21 +8,6 @@ CLUSTERADM=${CLUSTERADM:-clusteradm}
 KUBECTL=${KUBECTL:-kubectl}
 _managed_cluster_name="local-cluster"
 
-rm -rf clusteradm
-echo "############  Cloning clusteradm"
-git clone https://github.com/open-cluster-management-io/clusteradm.git
-
-cd clusteradm || {
-  printf "cd failed, clusteradm does not exist"
-  return 1
-}
-
-make install
-if [ $? -ne 0 ]; then
- echo "############  Failed to install clusteradm"
- exit 1
-fi
-
 $CLUSTERADM init --output-join-command-file join.sh --wait
 sh -c "$(cat join.sh) $_managed_cluster_name"
 $CLUSTERADM accept --clusters $_managed_cluster_name --wait 60
