91e7f8d8bbde1e45a06a33bbce1d9d5d3e9faf79
Roke Jung <roke@redhat.com>
2023-07-26 16:28:31 -0400

    remove commented out code
    Signed-off-by: Roke Jung <roke@redhat.com>

--- pkg/manager/cli_download_install.go
+++ pkg/manager/cli_download_install.go
@@ -309,41 +309,6 @@ func getConsoleDownload(routeUrl string, log logr.Logger) (*consolev1.ConsoleCLI
 		return nil, err
 	}
 
-	/*links := []consolev1.CLIDownloadLink{
-		{
-			Href: "https://" + routeUrl + "/linux/amd64/hypershift.tar.gz",
-			Text: "Download hypershift CLI for Linux for x86_64",
-		},
-		{
-			Href: "https://" + routeUrl + "/darwin/amd64/hypershift.tar.gz",
-			Text: "Download hypershift CLI for Mac for x86_64",
-		},
-		{
-			Href: "https://" + routeUrl + "/windows/amd64/hypershift.tar.gz",
-			Text: "Download hypershift CLI for Windows for x86_64",
-		},
-		{
-			Href: "https://" + routeUrl + "/linux/arm64/hypershift.tar.gz",
-			Text: "Download hypershift CLI for Linux for ARM 64",
-		},
-		{
-			Href: "https://" + routeUrl + "/darwin/arm64/hypershift.tar.gz",
-			Text: "Download hypershift CLI for Mac for ARM 64",
-		},
-		{
-			Href: "https://" + routeUrl + "/linux/ppc64/hypershift.tar.gz",
-			Text: "Download hypershift CLI for Linux for IBM Power",
-		},
-		{
-			Href: "https://" + routeUrl + "/linux/ppc64le/hypershift.tar.gz",
-			Text: "Download hypershift CLI for Linux for IBM Power, little endian",
-		},
-		{
-			Href: "https://" + routeUrl + "/linux/s390x/hypershift.tar.gz",
-			Text: "Download hypershift CLI for Linux for IBM Z",
-		},
-	}*/
-
 	links := []consolev1.CLIDownloadLink{}
 
 	links = append(links, consolev1.CLIDownloadLink{
